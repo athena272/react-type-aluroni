a1972364462406d41275f58d0e0d8b7b60d7229b
athena272 <guilhermera272@gmail.com>
2024-06-28 01:42:24 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable react/no-deprecated */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cardapio />
+    <Inicio />
   </React.StrictMode>,
   document.getElementById('root')
 );
