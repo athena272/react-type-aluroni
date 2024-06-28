9337b789987bc2c0c9ef2c45cc05ce02dd9c7530
athena272 <guilhermera272@gmail.com>
2024-06-28 02:16:55 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
-/* eslint-disable react/no-deprecated */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
-import Inicio from 'pages/Inicio';
+import AppRouter from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Inicio />
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
