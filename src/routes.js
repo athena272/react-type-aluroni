b72c5c47d219d3dee5df7c1e61dab30e09fd31ce
athena272 <guilhermera272@gmail.com>
2024-06-29 14:59:15 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,21 @@
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import Menu from 'components/Menu';
+import Header from 'components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='/cardapio' element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<Header />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }
