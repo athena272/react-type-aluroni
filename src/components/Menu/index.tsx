c18d83dda6d6d5508cb9d286df4be077977c49f4
athena272 <guilhermera272@gmail.com>
2024-06-29 11:23:04 -0300

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,16 +5,27 @@ export default function Menu() {
     const rotas = [{
         label: 'Início',
         to: '/'
-    }, {
+    },
+    {
         label: 'Cardápio',
         to: '/cardapio'
-    }, {
+    },
+    {
         label: 'Sobre',
         to: '/sobre'
     }];
     return (
         <nav className={styles.menu}>
             <Logo />
+            <ul className={styles.menu__list}>
+                {rotas.map((rota, index) => (
+                    <li key={index} className={styles.menu__link}>
+                        <a href={rota.to}>
+                            {rota.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </nav>
 
     );
