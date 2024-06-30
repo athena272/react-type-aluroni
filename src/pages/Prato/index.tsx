17ea4ba1f98479b58ef682fe6a14c6104b6eeef6
athena272 <guilhermera272@gmail.com>
2024-06-30 00:04:47 -0300

    Create index.tsx

---
+++ src/pages/Prato/index.tsx
@@ -0,0 +1,43 @@
+import styles from './Prato.module.scss';
+import { useParams, useNavigate } from 'react-router-dom';
+import cardapio from 'data/cardapio.json';
+import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+
+export default function Prato() {
+    // console.log(useLocation());
+    // const { state } = useLocation();
+    // const prato = state as typeof cardapio[0];
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const prato = cardapio.find(item => item.id === Number(id));
+    if (!prato) {
+        return <NotFound />;
+    }
+    return (
+        <>
+            <button
+                className={styles.voltar}
+                onClick={() => navigate(-1)}
+            >
+                {'< Voltar'}
+            </button>
+            <section className={styles.container}>
+                <h1 className={styles.titulo}>
+                    {prato.title}
+                </h1>
+                <div className={styles.imagem}>
+                    <img src={prato.photo} alt={prato.title} />
+                </div>
+                <div className={styles.conteudo}>
+                    <p className={styles.conteudo__descricao}>
+                        {prato.description}
+                    </p>
+                    <TagsPrato 
+                        prato={prato}
+                    />
+                </div>
+            </section>
+        </>
+    );
+}
